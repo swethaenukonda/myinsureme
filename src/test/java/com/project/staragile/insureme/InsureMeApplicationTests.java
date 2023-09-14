30e8c2fd0a4381992e50f54ddeb2596d7ad36594
swetha <swethamba859@gmail.com>
2023-09-14 16:21:35 +0530

    new addition

--- src/test/java/com/project/staragile/insureme/InsureMeApplicationTests.java
+++ src/test/java/com/project/staragile/insureme/InsureMeApplicationTests.java
@@ -14,7 +14,7 @@ class InsureMeApplicationTests {
 	
 	@Test
 	void testCreatePolicy() {
-		Policy policy = new Policy(1, "Shubham", "Individual" , 10000, "10-Sep-2021", "10-Sep-2022");
+		Policy policy = new Policy(1, "Swetha", "Individual" , 10000, "10-Sep-2021", "10-Sep-2022");
 		PolicyService pService = new PolicyService();
 		//Policy outputPolicy = pService.CreatePolicy();
 		assertEquals(policy.getPolicyId(), pService.generateDummyPolicy().getPolicyId());
